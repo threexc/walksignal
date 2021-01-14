4cbe84227abdfdede39476d82cc08342617cbd2d
threexc <tvgamblin@gmail.com>
2021-01-13 21:30:26 -0500

    Add cell tower reference data and method to retrieve it
    Signed-off-by: threexc <tvgamblin@gmail.com>

---
+++ data/oci_ref/README.md
@@ -0,0 +1,3 @@
+The data found here is found at https://www.opencellid.org/downloads.php
+
+OpenCelliD Project is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License 
--- walksignal/towers.py
+++ walksignal/towers.py
@@ -4,12 +4,16 @@ import sys
 import numpy as np
 
 class Tower:
-    def __init__(self, mcc, mnc, lac, cellid):
+    def __init__(self, signal_type, mcc, mnc, lac, cellid, lon, lat, tower_range, samples):
+        self.mcc = mcc
+        self.mnc = mnc
+        self.lac = lac
+        self.cellid = cellid
         self.lat = lat
         self.lon = lon
         self.range = tower_range
         self.samples = samples
-        self.type = signal_type
+        self.signal_type = signal_type
 
 class TowerSet:
     def __init__(self):
@@ -20,7 +24,7 @@ class TowerSet:
         self.lac_list = []
         self.cellid_list = []
 
-    def get_tower_info(self, data_file):
+    def __get_tower_info(self, data_file):
         self.data = read_csv(data_file)
         self.data_matrix = np.array(self.data)
         
@@ -40,7 +44,15 @@ class TowerSet:
             elif row[3] not in self.cellid_list:
                 self.cellid_list.append(row[3])
                 self.tower_id_list.append((row[0], row[1], row[2], row[3]))
-                
+
+    def get_tower_characteristics(self, data_file, reference_file):
+        self.__get_tower_info(data_file)
+        reference_data = read_csv(reference_file)
+        for row in reference_data:
+            for entry in self.tower_id_list:
+                if ((row[1] == entry[0]) and (row[2] == entry[1]) and (row[3] == entry[2]) and (row[4] == entry[3])):
+                    self.tower_list.append(Tower(row[0], row[1], row[2], row[3], row[4], row[6], row[7], row[8], row[9]))
+                    
 def read_csv(data_file):
     with open(data_file) as csv_file:
         data = list(csv.reader(csv_file, delimiter=","))
@@ -49,6 +61,15 @@ def read_csv(data_file):
 
 if __name__ == "__main__":
     towerset = TowerSet()
-    towerset.get_tower_info(sys.argv[1])
+    towerset.get_tower_characteristics(sys.argv[1], sys.argv[2])
     print(towerset.tower_id_list)
+    print(towerset.tower_list[0].mcc)
+    print(towerset.tower_list[0].mnc)
+    print(towerset.tower_list[0].lac)
+    print(towerset.tower_list[0].cellid)
+    print(towerset.tower_list[0].lat)
+    print(towerset.tower_list[0].lon)
+    print(towerset.tower_list[0].range)
+    print(towerset.tower_list[0].samples)
+    print(towerset.tower_list[0].signal_type)
 
