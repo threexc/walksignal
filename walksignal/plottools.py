96f79366e00686935a1f6e184060359e549baa69
threexc <tvgamblin@gmail.com>
2021-01-26 08:37:34 -0500

    Add plot_rating. All binaries will be combined in a later commit
    Signed-off-by: threexc <tvgamblin@gmail.com>

--- walksignal/plottools.py
+++ walksignal/plottools.py
@@ -62,6 +62,54 @@ def plot_gsp(datafile, reference_file):
 
     plt.show()
 
+def plot_rating(datafile, reference_file):
+    figs = {}
+    axs = {}
+    signal_data = np.array([])
+    tower_list = []
+    tower_lat_data = np.array([])
+    tower_lon_data = np.array([])
+    dataset = None
+    plottools = None
+    map_bbox = None
+    towerset = None
+
+    dataset = data.DataSet(datafile)
+    towerlist = towers.TowerList(datafile, reference_file)
+    
+
+    tower_lat_data, tower_lon_data = get_tower_positions(towerlist.tower_list)
+    plot_map, map_bbox = get_map_and_bbox(dataset.map_path, utils.get_bbox(dataset.bbox_path)[0])
+    
+    lat_data = np.array(dataset.data_matrix[1:,4], dtype=float)
+    lon_data = np.array(dataset.data_matrix[1:,5], dtype=float)
+    signal_data = np.array(dataset.data_matrix[1:,6], dtype=float)
+    np.set_printoptions(threshold=sys.maxsize)
+    print(dataset.rating)
+
+    # Plot the data on the map
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    im = ax1.imshow(plot_map, zorder=0, extent = map_bbox, aspect = "equal")
+    cm = plt.cm.get_cmap('gist_heat')
+    plot = ax1.scatter(lon_data, lat_data, zorder=1, alpha=1.0, c=dataset.rating, cmap=cm, s=40)
+    ax1.scatter(tower_lon_data, tower_lat_data, zorder=1, alpha=1.0, color="blue")
+    plt.xlim(map_bbox[0], map_bbox[1])
+    plt.ylim(map_bbox[2], map_bbox[3])
+    plt.ylabel("Latitude", rotation=90)
+    plt.xlabel("Longitude", rotation=0)
+    plt.title("Rating vs Position")
+    ax = plt.axes()
+
+    # Make sure to prevent lat/long from being displayed in scientific
+    # notation
+    ax.ticklabel_format(useOffset=False)
+    cax = fig.add_axes([ax.get_position().x1+0.01,ax.get_position().y0,0.02,ax.get_position().height])
+    cbar = plt.colorbar(plot, cax = cax)
+    cbar.ax.set_ylabel("GPS Rating (m)", rotation=270, labelpad=10)
+
+    plt.show()
+
 def plot_data(x_axis, y_axis, annotation=None, x_label="X", y_label="Y", plot_title="X vs Y"):
     scatter = plt.scatter(x_axis, y_axis, c = annotation, s = 2)
     if annotation is not None:
